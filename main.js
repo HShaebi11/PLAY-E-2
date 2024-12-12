4e84ba8b5c80918aece03916585efa9d31d3060d
HShaebi11 <hshaebi.11@gmail.com>
2024-12-12 00:42:31 +0000

    n

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ let model; // Define model variable to use it in animation
 
 // Load the 3D model
 loader.load(
-    'YOUR_MODEL_URL.glb', // Replace with your model URL
+    'https://raw.githubusercontent.com/HShaebi11/PLAY-E-2/refs/heads/main/smile.gltf', // Replace with your model URL
     function (gltf) {
         model = gltf.scene;
         // Center the model
